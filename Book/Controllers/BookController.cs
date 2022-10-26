dd32d8beb45a13447513ea9c044a4d257de927d8
unknown <Taras.Ivanyk@microsoft.wsei.edu.pl>
2022-10-26 09:40:44 +0100

    last_changes_from_lesson

---
+++ Book/Controllers/BookController.cs
@@ -0,0 +1,81 @@
+﻿using Book.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book.Controllers
+{
+
+    public class BookController : Controller
+    {
+        public static string urlImg = "https://assets.reedpopcdn.com/pok-mon-go-jak-zdoby-pikachu-jako-startowego-pokemona-14689433339.png/BROK/resize/690%3E/format/jpg/quality/75/pok-mon-go-jak-zdoby-pikachu-jako-startowego-pokemona-14689433339.png";
+        
+        public static int booksId = 0;
+        
+        private static List<BookViewModel> _books = new List<BookViewModel>()
+        {
+            new BookViewModel(){
+                Id = booksId++, 
+                Author = "Canady Trump",
+                DateOfPublication = new DateTime(2002,08,1).Date,
+                PageNumber = 600,
+                Img = urlImg,
+                Price = 6.00,
+                PubHouse = "MyHouse",
+                Title="Green Book",
+                Types = BookType.Anime 
+            },
+            new BookViewModel(){
+                Id = booksId++, 
+                Author = "Canady Trump",
+                DateOfPublication = new DateTime(2002,08,1).Date,
+                PageNumber = 600,
+                Img = urlImg,
+                Price = 6.00,
+                PubHouse = "MyHouse",
+                Title="Green Book",
+                Types = BookType.Anime 
+            },
+            new BookViewModel(){
+                Id = booksId++,
+                Author = "Canady Trump",
+                DateOfPublication = new DateTime(2002,08,1).Date,
+                PageNumber = 600,
+                Img = urlImg,
+                Price = 6.00,
+                PubHouse = "MyHouse",
+                Title="Green Book",
+                Types = BookType.Anime
+            },
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [HttpPost]
+        public IActionResult BookForm(BookViewModel bookViewModel)
+        {
+
+            BookViewModel book = new BookViewModel();
+            if (ModelState.IsValid)
+            {
+                _books.Add(bookViewModel);
+                return View(book);
+            }
+            else
+            {
+
+                return View();
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult EditForm(int id) {
+
+            return View();
+        }
+        
+    }
+
+    
+}
---
+++ Book/Models/BookViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Book.Models
+{
+    
+    public class BookViewModel
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        public string? Img { get; set; }
+        [Required]
+        [StringLength(40, ErrorMessage = "Zadługie tytół ")]
+        public string Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? DateOfPublication { get; set; }
+
+        [Required(ErrorMessage ="Podaj liczbe stron")]
+        public int PageNumber { get; set; }
+        public string? Author { get; set; }
+        public string? PubHouse { get; set; }
+        public BookType Types { get; set; }
+        public double Price { get; set; }
+
+
+    }
+
+    public enum BookType
+    {
+        [Display(Name = "Sci–Fi")] SciFi = 0,
+        [Display(Name = "Romans")] Roman = 1,
+        [Display(Name = "Horror")] Horror = 2,
+        [Display(Name = "Anime")] Anime = 3,
+    }
+
+
+}
